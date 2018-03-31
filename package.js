8b44083865ad4cf2e5850ab89a6a16e2f8179714
Timothy Pfafman <tim@pfafman.com>
2018-03-31 07:11:38 -0600

    constraints

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "meteorstuff:materialize-modal",
   summary: "Display a modal via Materialize written in coffeescript",
-  version: "1.1.6",
+  version: "1.1.7",
   git: "https://github.com/MeteorStuff/meteor-materialize-modal.git"
 });
 
 Package.onUse(function(api, where) {
-  api.versionsFrom("METEOR@1.4");
+  //api.versionsFrom("METEOR@1.5");
 
   api.use([
     'underscore',
